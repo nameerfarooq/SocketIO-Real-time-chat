9ec37caf4173d27363c7148a3fe5c54d84c7756f
Nameerfarooq <nameerfarooquit@gmail.com>
2024-06-06 14:52:02 +0500

    socket emit with broadcast

--- server/app.js
+++ server/app.js
@@ -1,10 +1,24 @@
 import express from "express";
-
+import { Server } from "socket.io";
+import { createServer } from "http";
 const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
 const port = 3000;
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
-app.listen(port, () => {
+io.on("connection", (socket) => {
+  console.log("User connected: ", socket.id);
+  socket.emit("welcome", `Welcome to the server `);
+  socket.broadcast.emit("welcome", `${socket.id} joined the server`);
+});
+server.listen(port, () => {
   console.log(`Server started at port ${port}`);
 });
