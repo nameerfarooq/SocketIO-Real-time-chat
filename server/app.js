5fe427fbb0fe053e43241b1d9c9e1e2a9384fa90
Nameerfarooq <nameerfarooquit@gmail.com>
2024-06-26 15:01:00 +0500

    done basic tutorial

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { Server } from "socket.io";
 import { createServer } from "http";
+import jwt from "jsonwebtoken";
 const app = express();
 const server = createServer(app);
+const secretKeyJWT = "asdasdasdasdasdasf";
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:5173",
@@ -13,6 +15,12 @@ const io = new Server(server, {
 const port = 3000;
 app.get("/", (req, res) => {
   res.send("Hello world");
+});
+app.get("/login", (req, res) => {
+  const token = jwt.sign({ _id: "asdasdasdavadv" }, secretKeyJWT);
+  res
+    .cookie("token", token, { httpOnly: true, secure: true, sameSite: "none" })
+    .json({ message: "Login Success" });
 });
 const user = false;
 io.use((socket, next) => {
